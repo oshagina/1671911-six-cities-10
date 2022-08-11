bb0f0abad45f7b460017abbcdcfc0371ad018c8e
oshagina6 <shaginao04@gmail.com>
2022-08-11 15:02:05 +0200

    refractoring

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
+//создаем root элемент
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
+  //строгий режим для реакта
   <React.StrictMode>
-    <App />
+    <App placeCardCount={3}/>
   </React.StrictMode>,
 );
